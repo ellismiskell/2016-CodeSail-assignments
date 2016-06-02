0b155919c5fb9911ae46e9676990c716a4f28513
ellismiskell <ellis.burgoonmiskell@yale.edu>
2016-06-02 15:27:08 -0400

    add hospital subclass

--- building.rb
+++ building.rb
@@ -13,3 +13,16 @@ end
 building_1 = Building.new(15, 4, 4, "metal", "gray", false)
 
 building_2 = Building.new(12, 3, 4, ["metal", "cement"], ["gray", "tan"], true)
+
+class Hospital < Building
+  attr_accessor :num_patients
+  def initialize(num_patients, window_count, wall_count, story_count, materials, colors, aircon)
+    super(window_count, wall_count, story_count, materials, colors, aircon)
+    @num_patients = num_patients
+  end
+  def make_sound
+    puts "ambulance siren WOO WOO"
+  end
+end
+
+h1 = Hospital.new(700, 400, 4, 8, ["wood", "metal"], "blue", true)
